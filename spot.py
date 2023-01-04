f03393c15e6cbc1b0b1f134f0614edd7acacfa86
Hannah Lormenyo <lormenyoh@gmail.com>
2023-01-04 20:14:52 +0000

    updated with code

--- spot.py
+++ spot.py
@@ -1,8 +1,13 @@
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
+import json
 
-sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id="YOUR_APP_CLIENT_ID",
-                                                           client_secret="YOUR_APP_CLIENT_SECRET"))
+configFile = open("config.json")
+
+config = json.load(configFile)
+
+sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id=config["clientId"],
+                                                           client_secret=config["clientSecret"]))
 
 results = sp.search(q='weezer', limit=20)
 for idx, track in enumerate(results['tracks']['items']):
