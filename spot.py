3dfc9c89d1d67738fd6fee42b6bbd97e102da1e0
Hannah Lormenyo <lormenyoh@gmail.com>
2023-01-05 23:07:53 +0000

    added class

--- spot.py
+++ spot.py
@@ -25,6 +25,38 @@ artist = genius.search_artist("Sam Kim", max_songs=3, sort="title", include_feat
 print(artist.songs)
 
 song = genius.search_song("Breath", artist.name)
-print(song.lyrics)
+lyrics = song.lyrics
 
-print(ts.translate_text(song.lyrics, if_ignore_empty_query=False, if_ignore_limit_of_length=False, limit_of_length=5000))
+translation = ts.translate_text(song.lyrics,from_language='ko', to_language='en', if_ignore_empty_query=False, if_ignore_limit_of_length=False, limit_of_length=5000)
+
+lyrisList = lyrics.split("\n\n")
+translationList = translation.split("\n\n")
+
+for l in lyrisList:
+    for t in translationList:
+        print(l)
+        print("-----")
+        print(t)
+        print("\n")
+
+# print(lyrislist)
+# print(len(lyrislist))
+
+class SpotifySong:
+    def __init__(self, songName) -> None:
+        self.songName = songName
+
+    def getSongArtist():
+        return
+
+    def getSongLyrics():
+        return
+
+    def getFullSongTranslation(fromLang, toLang):
+        return
+
+    def getLyricsAndTranslation(fromLang, toLang):
+        return
+
+    def getSongRomanization():
+        return 
