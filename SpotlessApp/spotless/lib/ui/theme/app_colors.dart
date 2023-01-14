b37e6b971f9d26d7d48b136c946aea2ae5803dab
Hannah Lormenyo <lormenyoh@gmail.com>
2023-01-14 22:28:48 +0000

    wowrk for more

---
+++ SpotlessApp/spotless/lib/ui/theme/app_colors.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  AppColors._();
+
+  static const spotlessBlue750 = Color(0xFF3647B3);
+
+  static const spotlessBlue700 = Color(0xFF303E9F);
+
+  static const spotlessBlue50 = Color(0xFFCFD4FA);
+
+  static const spotlessBlue100 = Color(0XFFEEEFFD);
+
+  static const spotlessBlue200 = Color(0xFFE7EAF8);
+
+  static const spotlessBlue300 = Color(0XFFD7DCF1);
+
+  static const spotlessDarkBlue = Color(0XFF050A30);
+
+  static const spotlessDarkBg = Color(0xFF121212);
+
+  static const spotlessDark300 = Color(0xFF0D0D0D);
+
+  static const spotlessDark300a60 = Color.fromRGBO(13, 13, 13, 0.6);
+
+  static const spotlessDark400 = Color(0xFF2F2F2F);
+
+  static const spotlessDark500 = Color(0xFF0D112B);
+
+  static const spotlessYellow100 = Color(0XFFFFF8E0);
+
+  static const spotlessYellow200 = Color(0XFFFFC720);
+
+  static const spotlessBrown100 = Color(0xFF382A00);
+
+  static const spotlessGrey50 = Color(0xFFDDDDDD);
+
+  static const spotlessGrey100 = Color(0XFF4F5161);
+
+  static const spotlessGrey200 = Color(0xFF7B7E95);
+
+  static const spotlessGrey250 = Color(0xFF7B7E95);
+
+  static const spotlessGrey300 = Color(0xFF5B5D6D);
+
+  static const spotlessPurple = Color(0xFFC85CE3);
+
+  static const spotlessWhite = Color(0xFFFFFFFF);
+
+  static const spotlessWhite20 = Color.fromRGBO(255, 255, 255, 0.2);
+
+  static const spotlessWhite40 = Color.fromRGBO(255, 255, 255, 0.4);
+
+  static const spotlessBlack = Color(0xFF000000);
+
+  static const spotlessGreyBg = Color(0xFFFAFAFA);
+
+  static const spotlessDeepBlueGradient = [
+    Color(0xff005C97),
+    Color.fromRGBO(54, 55, 149, 1)
+  ];
+
+  static const spotlessLightBlueGradient = [
+    Color.fromRGBO(0, 92, 151, 0.6),
+    Color.fromRGBO(54, 55, 149, 0.6)
+  ];
+}
--- SpotlessApp/spotless/lib/ui/theme/app_text_theme.dart
+++ SpotlessApp/spotless/lib/ui/theme/app_text_theme.dart
@@ -1,1 +1,102 @@
+import 'package:spotless/ui/theme/app_colors.dart';
+import 'package:flutter/material.dart';
 
+TextTheme spotlessLightTextTheme(TextTheme base) {
+  return base
+      .copyWith(
+        headline1: base.headline1!.copyWith(
+          fontSize: 32.0,
+          height: 1.25,
+          fontFamily: 'Satoshi',
+          fontWeight: FontWeight.w300,
+        ),
+        headline2: base.headline2!.copyWith(
+          fontSize: 20.0,
+          height: 1.2,
+          fontFamily: 'Satoshi',
+        ),
+        headline3: base.headline3!.copyWith(
+          fontSize: 16.0,
+          height: 1.125,
+          fontFamily: 'Satoshi',
+        ),
+        headline4: base.headline4!.copyWith(
+          fontSize: 14.0,
+          height: 1.14,
+          fontFamily: 'Satoshi',
+        ),
+        subtitle1: base.headline3!.copyWith(
+          fontSize: 14.0,
+          height: 1.2,
+          fontFamily: 'Satoshi',
+        ),
+        bodyText1: base.bodyText1!.copyWith(
+          fontSize: 14.0,
+          height: 1.2,
+          fontFamily: 'Satoshi',
+        ),
+        bodyText2: base.bodyText2!.copyWith(
+          fontSize: 12.0,
+          height: 1.17,
+          fontFamily: 'Satoshi',
+        ),
+        button: base.button!.copyWith(
+          fontSize: 14.0,
+          height: 1.2,
+          fontFamily: 'Satoshi',
+        ),
+        caption: base.caption!.copyWith(
+          fontSize: 10.0,
+          height: 1.2,
+          fontFamily: 'Satoshi',
+        ),
+        overline: base.overline!
+            .copyWith(fontWeight: FontWeight.normal, fontFamily: 'Satoshi'),
+      )
+      .apply(
+        displayColor: AppColors.spotlessBlack,
+        bodyColor: AppColors.spotlessBlack,
+      );
+}
+
+extension CustomspotlessText on TextTheme {
+  TextStyle get spotlessTitle900 {
+    return const TextStyle(
+      fontSize: 20,
+      fontWeight: FontWeight.w700,
+      fontFamily: 'Satoshi',
+    );
+  }
+
+  TextStyle get spotlessTitle700 {
+    return const TextStyle(
+      fontSize: 14,
+      fontWeight: FontWeight.w700,
+      fontFamily: 'Satoshi',
+    );
+  }
+
+  TextStyle get spotlessTitle500 {
+    return const TextStyle(
+      fontSize: 14,
+      fontWeight: FontWeight.w500,
+      fontFamily: 'Satoshi',
+    );
+  }
+
+  TextStyle get spotlessBody100 {
+    return const TextStyle(
+      fontSize: 12,
+      fontWeight: FontWeight.normal,
+      fontFamily: 'Satoshi',
+    );
+  }
+
+  TextStyle get spotlessBody700 {
+    return const TextStyle(
+      fontSize: 14,
+      fontWeight: FontWeight.normal,
+      fontFamily: 'Satoshi',
+    );
+  }
+}
