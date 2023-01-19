fed1ccb0275717b3f20bf2269a22316be9ae466e
Hannah Lormenyo <lormenyoh@gmail.com>
2023-01-19 22:51:37 +0000

    prelims

--- SpotlessApp/spotless/lib/ui/screens/home.dart
+++ SpotlessApp/spotless/lib/ui/screens/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 class Home extends StatefulWidget {
   const Home({Key? key, required this.title}) : super(key: key);
@@ -13,14 +14,52 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: const <Widget>[
-            Text(
-              'Home page',
-            )
-          ],
+        child: Container(
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              const SizedBox(
+                height: 50,
+              ),
+              getSearchBar(),
+              getPageTitle()
+            ],
+          ),
         ),
+      ),
+    );
+  }
+
+  Widget getSearchBar() {
+    return SizedBox(
+      width: MediaQuery.of(context).size.width,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: const [
+          CircleAvatar(),
+          CircleAvatar(
+            backgroundColor: Colors.blue,
+            child: Icon(Icons.search),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget getPageTitle() {
+    return SizedBox(
+      width: MediaQuery.of(context).size.width,
+      child: Row(
+        children: [
+          SizedBox(width: 50, child: const Text('Find the best music for you')),
+          SizedBox(
+              width: 100,
+              child: SvgPicture.asset('assets/icons/hand-point-right.svg'))
+        ],
       ),
     );
   }
