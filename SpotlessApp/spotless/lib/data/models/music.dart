a9915d575cba08dcb0bb9573182e4a9201af7e6b
Hannah Lormenyo <lormenyoh@gmail.com>
2023-01-19 22:23:19 +0000

    added music model

---
+++ SpotlessApp/spotless/lib/data/models/music.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class Music {
+  String artUrl;
+  String title;
+  String artistName;
+  String genre;
+
+  Music(
+      {required this.artUrl,
+      required this.title,
+      required this.genre,
+      required this.artistName});
+}
--- SpotlessApp/spotless/lib/ui/widgets/music_card.dart
+++ SpotlessApp/spotless/lib/ui/widgets/music_card.dart
@@ -1,9 +1,20 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/foundation/key.dart';
-import 'package:flutter/src/widgets/framework.dart';
+import 'package:spotless/data/models/music.dart';
 
-class MusicCard extends StatelessWidget {
-  const MusicCard({Key? key}) : super(key: key);
+class SquareMusicCard extends StatelessWidget {
+  // art_url, title, artist, genre
+  const SquareMusicCard({Key? key, required Music music}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
+class RectangularMusicCard extends StatelessWidget {
+  // art_url, title, artist, genre
+  const RectangularMusicCard({Key? key, required Music music})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
