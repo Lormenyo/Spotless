a0b655f9aba82e589ab59757bcc45b11992a4549
Hannah Lormenyo <lormenyoh@gmail.com>
2023-01-22 22:14:28 +0000

    added assets

--- SpotlessApp/spotless/lib/ui/theme/app_assets.dart
+++ SpotlessApp/spotless/lib/ui/theme/app_assets.dart
@@ -1,3 +1,7 @@
 const kSearchIcon = 'assets/icons/search.svg';
 
 const kRightHandPoint = 'assets/images/hand-point-right.png';
+
+const kProfileImage = 'assets/images/profile.png';
+
+const kCoverArt = 'assets/images/cover-art.png';
