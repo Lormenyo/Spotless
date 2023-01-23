14491fa8afdc8fb66a1b665da6b5b66c2a9f0088
Hannah Lormenyo <lormenyoh@gmail.com>
2023-01-23 22:41:56 +0000

    added text

--- SpotlessApp/spotless/lib/ui/widgets/music_card.dart
+++ SpotlessApp/spotless/lib/ui/widgets/music_card.dart
@@ -31,9 +31,30 @@ class SquareMusicCard extends StatelessWidget {
             child: Container(
               height: 70,
               width: 120,
+              padding: const EdgeInsets.all(10.0),
               decoration: const BoxDecoration(
                   color: Colors.white,
-                  borderRadius: BorderRadius.all(Radius.circular(10.0))),
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(10.0),
+                  ),
+                  gradient: LinearGradient(
+                    colors: [
+                      Color.fromRGBO(
+                        255,
+                        255,
+                        255,
+                        1.0,
+                      ),
+                      Color.fromRGBO(255, 255, 255, 0.62),
+                      Color.fromRGBO(255, 255, 255, 0.28)
+                    ],
+                    stops: [0.0, 0.5, 1.0],
+                  )),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [Text(music.title), Text(music.genre)],
+              ),
             ),
           )
         ],
