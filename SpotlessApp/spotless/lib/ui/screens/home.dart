4137a8e3e86a8f89cff3f05c2f526d913fb9b65c
Hannah Lormenyo <lormenyoh@gmail.com>
2023-01-29 22:06:00 +0000

    removed unused import

--- SpotlessApp/spotless/lib/ui/screens/home.dart
+++ SpotlessApp/spotless/lib/ui/screens/home.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:spotless/data/models/music.dart';
 import 'package:spotless/ui/theme/app_assets.dart';
