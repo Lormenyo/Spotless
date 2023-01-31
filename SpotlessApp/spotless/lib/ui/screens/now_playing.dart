1041ef71850659c241d09049b36b5eee38894dd7
Hannah Lormenyo <lormenyoh@gmail.com>
2023-01-31 22:01:49 +0000

    added now playing

---
+++ SpotlessApp/spotless/lib/ui/screens/now_playing.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class NowPlaying extends StatefulWidget {
+  const NowPlaying({Key? key}) : super(key: key);
+
+  @override
+  State<NowPlaying> createState() => _NowPlayingState();
+}
+
+class _NowPlayingState extends State<NowPlaying> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(),
+    );
+  }
+}
