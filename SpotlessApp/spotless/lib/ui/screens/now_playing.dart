5f05e4cd7e6923d0deb7ca4e0a29adba7a254f6c
Hannah Lormenyo <lormenyoh@gmail.com>
2023-02-03 22:20:25 +0000

    added more to layout

--- SpotlessApp/spotless/lib/ui/screens/now_playing.dart
+++ SpotlessApp/spotless/lib/ui/screens/now_playing.dart
@@ -21,13 +21,17 @@ class _NowPlayingState extends State<NowPlaying> {
         width: MediaQuery.of(context).size.width,
         height: MediaQuery.of(context).size.height,
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             const SizedBox(
               height: 60,
             ),
-            NowPlayingMusicCard(
-              music: widget.music,
-            )
+            Center(
+              child: NowPlayingMusicCard(
+                music: widget.music,
+              ),
+            ),
+            buildMusicSeekBar()
           ],
         ),
       ),
@@ -53,6 +57,22 @@ class _NowPlayingState extends State<NowPlaying> {
             ?.copyWith(color: Colors.black, fontSize: 25),
       ),
       backgroundColor: Colors.white,
+    );
+  }
+
+  buildMusicSeekBar() {
+    return Container(
+      padding: const EdgeInsets.all(20.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const SizedBox(
+            height: 30,
+          ),
+          Text(widget.music.title)
+        ],
+      ),
     );
   }
 }
