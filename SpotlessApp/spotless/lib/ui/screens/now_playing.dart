df0b840b9b2b7005eac47ff18157c413a09931c5
Hannah Lormenyo <lormenyoh@gmail.com>
2023-02-03 23:07:52 +0000

    added seck bar

--- SpotlessApp/spotless/lib/ui/screens/now_playing.dart
+++ SpotlessApp/spotless/lib/ui/screens/now_playing.dart
@@ -2,6 +2,7 @@ import 'package:audio_session/audio_session.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:just_audio/just_audio.dart';
+import 'package:rxdart/rxdart.dart';
 import 'package:spotless/data/models/music.dart';
 import 'package:spotless/ui/theme/app_colors.dart';
 import 'package:spotless/ui/widgets/music_card.dart';
@@ -44,6 +45,16 @@ class _NowPlayingState extends State<NowPlaying> with WidgetsBindingObserver {
       print("Error loading audio source: $e");
     }
   }
+
+  /// Collects the data useful for displaying in a seek bar, using a handy
+  /// feature of rx_dart to combine the 3 streams of interest into one.
+  Stream<PositionData> get _positionDataStream =>
+      Rx.combineLatest3<Duration, Duration, Duration?, PositionData>(
+          _audioPlayer.positionStream,
+          _audioPlayer.bufferedPositionStream,
+          _audioPlayer.durationStream,
+          (position, bufferedPosition, duration) => PositionData(
+              position, bufferedPosition, duration ?? Duration.zero));
 
   @override
   Widget build(BuildContext context) {
@@ -118,6 +129,22 @@ class _NowPlayingState extends State<NowPlaying> with WidgetsBindingObserver {
                 .textTheme
                 .bodyText2
                 ?.copyWith(fontSize: 17, color: AppColors.spotlessGray3),
+          ),
+          // ControlButtons(_player),
+          // Display seek bar. Using StreamBuilder, this widget rebuilds
+          // each time the position, buffered position or duration changes.
+          StreamBuilder<PositionData>(
+            stream: _positionDataStream,
+            builder: (context, snapshot) {
+              final positionData = snapshot.data;
+              return SeekBar(
+                duration: positionData?.duration ?? Duration.zero,
+                position: positionData?.position ?? Duration.zero,
+                bufferedPosition:
+                    positionData?.bufferedPosition ?? Duration.zero,
+                onChangeEnd: _audioPlayer.seek,
+              );
+            },
           ),
         ],
       ),
