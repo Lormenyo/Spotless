cd85ff597eea5027dc6f2e9e194c649417f99951
Hannah Lormenyo <lormenyoh@gmail.com>
2023-02-09 00:03:32 +0000

    removed unused

--- SpotlessApp/spotless/lib/ui/screens/now_playing.dart
+++ SpotlessApp/spotless/lib/ui/screens/now_playing.dart
@@ -253,39 +253,4 @@ class _NowPlayingState extends State<NowPlaying> with WidgetsBindingObserver {
       ),
     );
   }
-
-  void displayPersistentBottomSheet() {
-    Scaffold.of(context).showBottomSheet<void>((BuildContext context) {
-      return Column(mainAxisSize: MainAxisSize.min, children: <Widget>[
-        ListTile(
-          leading: Icon(Icons.person),
-          title: const Text(' My Profile '),
-          onTap: () {
-            Navigator.pop(context);
-          },
-        ),
-        ListTile(
-          leading: Icon(Icons.book),
-          title: const Text(' My Course '),
-          onTap: () {
-            Navigator.pop(context);
-          },
-        ),
-        ListTile(
-          leading: Icon(Icons.workspace_premium),
-          title: const Text(' Go Premium '),
-          onTap: () {
-            Navigator.pop(context);
-          },
-        ),
-        ListTile(
-          leading: Icon(Icons.video_label),
-          title: const Text(' Saved Videos '),
-          onTap: () {
-            Navigator.pop(context);
-          },
-        ),
-      ]);
-    });
-  }
 }
