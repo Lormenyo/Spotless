787b4d225ba23d2aa30600df5f8b2fef89c9c938
Hannah Lormenyo <lormenyoh@gmail.com>
2023-02-22 20:53:00 +0000

    added new

--- Spotless-be/app/models/spotifyModel.py
+++ Spotless-be/app/models/spotifyModel.py
@@ -1,37 +1,6 @@
-import spotipy
-from spotipy.oauth2 import SpotifyClientCredentials
-import json
-import lyricsgenius
+from spot import genius;
 import translators as ts
 from korean_romanizer.romanizer import Romanizer
-import pprint
-
-# this one is based on python 2, so not pretty nice
-# can use https://pythonhosted.org/doodle-translate/intro.html for 
-# translation but the installation is not working
-# it also has romanization and can detect other languages
-
-#this one is based on python 3, not so good either
-# https://pypi.org/project/googletrans/
-#from googletrans import Translator
-
-
-configFile = open("config.json")
-
-config = json.load(configFile)
-
-configFile.close()
-
-sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id=config["clientId"],
-                                                           client_secret=config["clientSecret"]))
-
-genius = lyricsgenius.Genius(config["lyricsAccessToken"])
-
-ser = sp.search('Shape of you', limit=1, offset=0, type='track', market=None)
-pprint.pprint(ser)
-
-# sp.start_playback() Premium is required for playback
-
 
 class SpotifySong:
     def __init__(self, songName:str, artistName:str) -> None:
@@ -67,8 +36,3 @@ class SpotifySong:
 
     def getLanguage(self):
         return
-        
-
-song = SpotifySong(songName='Breath', artistName='Sam Kim')
-#song.getLyricsAndTranslation('ko', 'en')
-# song.getKoreanSongRomanization()
