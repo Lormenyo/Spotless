7e6c10340a80404fc529d35dbc7172f3fd61d2b8
Hannah Lormenyo <lormenyoh@gmail.com>
2023-02-26 11:01:40 +0000

    necessary endpoints

--- Spotless-be/app/controllers/routes.py
+++ Spotless-be/app/controllers/routes.py
@@ -3,3 +3,22 @@ from flaskHelper import app
 @app.route('/')
 def index():
     return 'Welcome to Spotless API'
+
+@app.route('/search')
+def search():
+    """
+    can be songs/artists
+    """
+    return 'Search results'
+
+@app.route('/lyrics')
+def getLyrics():
+    return 'Lyrics'
+
+@app.route('/trending')
+def getTrending():
+    return 'Trending'
+
+@app.route('/new-releases')
+def getNewReleases():
+    return 'new releases'
