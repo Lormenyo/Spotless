dad875104e3531d3a44a24c89a0f1b3e5939ab95
Hannah Lormenyo <lormenyoh@gmail.com>
2023-03-01 22:01:28 +0000

    added changes

--- Spotless-be/app/flaskHelper.py
+++ Spotless-be/app/flaskHelper.py
@@ -1,4 +1,4 @@
-from flask import Flask, Blueprint
+from flask import Flask
 from flask_restx import Api
 from controllers.search import searchNamespace
 from controllers.lyrics import lyricsNamespace
