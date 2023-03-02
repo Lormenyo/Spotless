4089bca571146f4a7dd07ba525d40c5be7775a70
Hannah Lormenyo <lormenyoh@gmail.com>
2023-03-02 21:20:04 +0000

    updated with release model

--- SpotlessApp/spotless/lib/data/api/release.g.dart
+++ SpotlessApp/spotless/lib/data/api/release.g.dart
@@ -1,23 +1,44 @@
 // GENERATED CODE - DO NOT MODIFY BY HAND
 
-part of 'example.dart';
+part of 'release.dart';
 
 // **************************************************************************
 // JsonSerializableGenerator
 // **************************************************************************
 
-Task _$TaskFromJson(Map<String, dynamic> json) => Task(
+ReleaseAlbum _$ReleaseAlbumFromJson(Map<String, dynamic> json) => ReleaseAlbum(
+      album_type: json['album_type'] as String?,
+      artists: json['artists'] as List<dynamic>?,
+      available_markets: json['available_markets'] as List<dynamic>?,
+      external_urls: json['external_urls'] as Map<String, dynamic>?,
+      href: json['href'] as String?,
       id: json['id'] as String?,
+      images: (json['images'] as List<dynamic>?)
+          ?.map((e) => e as Map<String, dynamic>)
+          .toList(),
       name: json['name'] as String?,
-      avatar: json['avatar'] as String?,
-      createdAt: json['createdAt'] as String?,
+      release_date: json['release_date'] as String?,
+      release_date_precision: json['release_date_precision'] as String?,
+      total_tracks: json['total_tracks'] as int?,
+      type: json['type'] as String?,
+      uri: json['uri'] as String?,
     );
 
-Map<String, dynamic> _$TaskToJson(Task instance) => <String, dynamic>{
+Map<String, dynamic> _$ReleaseAlbumToJson(ReleaseAlbum instance) =>
+    <String, dynamic>{
+      'album_type': instance.album_type,
+      'artists': instance.artists,
+      'available_markets': instance.available_markets,
+      'external_urls': instance.external_urls,
+      'href': instance.href,
       'id': instance.id,
+      'images': instance.images,
       'name': instance.name,
-      'avatar': instance.avatar,
-      'createdAt': instance.createdAt,
+      'release_date': instance.release_date,
+      'release_date_precision': instance.release_date_precision,
+      'total_tracks': instance.total_tracks,
+      'type': instance.type,
+      'uri': instance.uri,
     };
 
 // **************************************************************************
@@ -39,26 +60,26 @@ class _RestClient implements RestClient {
   String? baseUrl;
 
   @override
-  Future<List<Task>> getTasks() async {
+  Future<List<ReleaseAlbum>> getNewReleases() async {
     const _extra = <String, dynamic>{};
     final queryParameters = <String, dynamic>{};
     final _headers = <String, dynamic>{};
     final _data = <String, dynamic>{};
-    final _result =
-        await _dio.fetch<List<dynamic>>(_setStreamType<List<Task>>(Options(
+    final _result = await _dio
+        .fetch<List<dynamic>>(_setStreamType<List<ReleaseAlbum>>(Options(
       method: 'GET',
       headers: _headers,
       extra: _extra,
     )
             .compose(
               _dio.options,
-              '/tasks',
+              '/release',
               queryParameters: queryParameters,
               data: _data,
             )
             .copyWith(baseUrl: baseUrl ?? _dio.options.baseUrl)));
     var value = _result.data!
-        .map((dynamic i) => Task.fromJson(i as Map<String, dynamic>))
+        .map((dynamic i) => ReleaseAlbum.fromJson(i as Map<String, dynamic>))
         .toList();
     return value;
   }
