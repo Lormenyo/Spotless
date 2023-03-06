c0c10316e34365fe0d9dfec09c4ea2e4d4b03fad
Hannah Lormenyo <lormenyoh@gmail.com>
2023-03-06 22:54:10 +0000

    removed unused import

--- Spotless-be/app.py
+++ Spotless-be/app.py
@@ -1,22 +1,7 @@
-import pprint
-from spotifyHelper import sp
-from models.spotifyModel import SpotifySong;
-
-import controllers.search
 from flaskHelper import app
 
 
 
-# def main():
-#     song = SpotifySong(songName='Breath', artistName='Sam Kim')
-    
-
-#     ser = sp.search('Shape of you', limit=1, offset=0, type='track', market=None)
-#     pprint.pprint(ser)
-
-    
-
-    
 
 
 if __name__ == '__main__':
