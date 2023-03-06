fe36ac524d6a469eb73cb0573db547a5c1ae0e5b
Hannah Lormenyo <lormenyoh@gmail.com>
2023-03-06 22:52:42 +0000

    update

--- Spotless-be/app.py
+++ Spotless-be/app.py
@@ -1,22 +1,8 @@
-import pprint
-from app.spotifyHelper import sp
-from app.models.spotifyModel import SpotifySong;
-
-import app.controllers.search
 from app.flaskHelper import app
 
 
 
-# def main():
-#     song = SpotifySong(songName='Breath', artistName='Sam Kim')
-    
-
-#     ser = sp.search('Shape of you', limit=1, offset=0, type='track', market=None)
-#     pprint.pprint(ser)
-
-    
-
-    
+   
 
 
 if __name__ == '__main__':
