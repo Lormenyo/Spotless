8f5863b02bb08e2c91708d3f2a6b0efd2b107cd4
Hannah Lormenyo <lormenyoh@gmail.com>
2023-03-10 08:32:54 +0000

    updated app

--- SpotlessApp/spotless/lib/data/api/release.dart
+++ SpotlessApp/spotless/lib/data/api/release.dart
@@ -4,7 +4,7 @@ import 'package:dio/dio.dart';
 
 part 'release.g.dart';
 
-@RestApi(baseUrl: "https://5d42a6e2bc64f90014a56ca0.mockapi.io/api/v1/")
+@RestApi(baseUrl: "https://spotless.up.railway.app/")
 abstract class RestClient {
   factory RestClient(Dio dio, {String baseUrl}) = _RestClient;
 
